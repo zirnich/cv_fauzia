51ea121c7b3b7c3b21304dec9fac89bd289235d0
fda <fauziabusiness@gmail.com>
2023-07-16 10:12:30 +0700

    cv

---
+++ app/Views/index.php
@@ -0,0 +1,449 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Fauzia Dewi Amalia</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="/assets/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="/assets/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="/assets/css/bootstrap.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="/assets/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="/assets/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">	
+	<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image:url(/assets/images/cover_bg_3.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t js-fullheight">
+						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+							<div class="profile-thumb" style="background: url(/assets/images/img.png);"></div>
+							<h1><span>Fauzia Dewi Amalia</span></h1>
+							<h3><span>Web Developer / Mobile Developer / Game Developer</span></h3>
+                            <!-- <h3><span>Microcontroller </span></h3> -->
+							<p>
+								<ul class="fh5co-social-icons">
+									<li><a href="#"><i class="icon-instagram2"></i></a></li>
+									<li><a href="https://github.com/zirnich"><i class="icon-github2"></i></a></li>
+									<li><a href="https://www.linkedin.com/in/fauzia-dewi-amalia-b96635266/"><i class="icon-linkedin2"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-about" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>About Me</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<ul class="info">
+						<li><span class="first-block">Full Name:</span><span class="second-block">Fauzia Dewi Amalia</span></li>
+						<li><span class="first-block">Phone:</span><span class="second-block">+62 8988284301</span></li>
+						<li><span class="first-block">Email:</span><span class="second-block">fauziadewi0401@gmail.com</span></li>
+						<li><span class="first-block">Address:</span><span class="second-block">198 West 21th Street, Suite 721 New York NY 10016</span></li>
+					</ul>
+				</div>
+				<div class="col-md-8">
+					<h2>Hello There!</h2>
+					<p>my name is fauzia dewi amalia i am a student from muhammadiyah university. have practical experience in field work at BPKD for three months in the program planning section. have an interest in the world of technology and seek new challenges.</p>
+					
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-instagram2"></i></a></li>
+							<li><a href="https://github.com/zirnich"><i class="icon-github2"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/fauzia-dewi-amalia-b96635266/"><i class="icon-linkedin2"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-resume" class="fh5co-bg-color">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>My Resume</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0">
+					<ul class="timeline">
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Experience</h3></div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">MSIB Complete Front-End Engineer Career With Flutter</h3>
+									<span class="company">Alterra Academy - feb 2023 - Current</span>
+								</div>
+								<div class="timeline-body">
+									<p>Participate in the Independent Campus Program Batch 4</p>
+								</div>
+							</div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">MSIB game developer industry class</h3>
+									<span class="company">Gamelab.id - aug 2022 - dec 2022</span>
+								</div>
+								<div class="timeline-body">
+									<p>Participate in the Independent Campus Program Batch 3</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-suitcase"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Field Practice(PKL)</h3>
+									<span class="company">BPKD Sukabumi - apr 2019 - jun 2019</span>
+								</div>
+								<div class="timeline-body">
+									<p>in the program planning section</p>
+								</div>
+							</div>
+						</li>
+
+						<br>
+						<li class="timeline-heading text-center animate-box">
+							<div><h3>Education</h3></div>
+						</li>
+						<li class="timeline-inverted animate-box">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Bachelors Degree</h3>
+									<span class="company">Muhammadiyah University - 2020 - Current</span>
+								</div>
+								<div class="timeline-body">
+									<p>Informatics Engineering</p>
+								</div>
+							</div>
+						</li>
+						<li class="animate-box timeline-unverted">
+							<div class="timeline-badge"><i class="icon-graduation-cap"></i></div>
+							<div class="timeline-panel">
+								<div class="timeline-heading">
+									<h3 class="timeline-title">Vocational School</h3>
+									<span class="company">SMK Pasim Plus - 2002 - 2006</span>
+								</div>
+								<div class="timeline-body">
+									<p>software engineering major</p>
+								</div>
+							</div>
+						</li>
+			    	</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="fh5co-features" class="animate-box">
+		<div class="container">
+			<div class="services-padding">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<h2>My Services</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-browser"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Web Developer</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-mobile"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Mobile Developer</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col-md-6 text-center">
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-windows"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Game Developer</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+					</div>
+                    <div class="col-md-6 text-center">
+
+						<div class="feature-left">
+							<span class="icon">
+								<i class="icon-network"></i>
+							</span>
+							<div class="feature-copy">
+								<h3>Microcontroller</h3>
+								<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-skills" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Skills</h2>
+				</div>
+			</div>
+			<div class="row row-pb-md">
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="95"><span><strong>HTML</strong>80%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="93"><span><strong>Construct 3</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>Dart</strong>85%</span></div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="89"><span><strong>PHP</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>Phaser</strong>75%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>C#</strong>75%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="85"><span><strong>Flutter</strong>85%</span></div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12 text-center">
+					<div class="chart" data-percent="90"><span><strong>Java</strong>75%</span></div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+	<div id="fh5co-work" class="fh5co-bg-dark">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Work</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/Picture1.png);">
+						<div class="desc">
+							<h3>game about the solar system</h3>
+							<span>Game</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/Picture2.JPG);">
+						<div class="desc">
+							<h3>space shooter games</h3>
+							<span>Game</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/Picture3.png);">
+						<div class="desc">
+							<h3>Health and Diet App</h3>
+							<span>Android App</span>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/drone.jpeg);">
+						<div class="desc">
+							<h3>Drone</h3>
+							<span>Microcontroller</span>
+						</div>
+					</a>
+				</div>
+                <div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/micro.jpg);">
+						<div class="desc">
+							<h3></h3>
+							<span>Microcontroller</span>
+						</div>
+					</a>
+				</div>
+                <div class="col-md-4 text-center col-padding animate-box">
+					<a href="#" class="work" style="background-image: url(/assets/images/Picture4.png);">
+						<div class="desc">
+							<h3>Zirnich Studio</h3>
+							<span>Android App</span>
+						</div>
+					</a>
+				</div>		
+				
+			</div>
+		</div>
+	</div>
+
+	
+
+	<div id="fh5co-consult">
+		<div class="video fh5co-video" style="background-image: url(/assets/images/cover_bg_1.jpg);">
+			<div class="overlay"></div>
+		</div>
+		<div class="choose animate-box">
+			<h2>Contact</h2>
+			<form action="#">
+				<div class="row form-group">
+					<div class="col-md-6">
+						<input type="text" id="fname" class="form-control" placeholder="Your firstname">
+					</div>
+				</div>
+				<div class="row form-group">
+					<div class="col-md-6">
+						<input type="text" id="lname" class="form-control" placeholder="Your lastname">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="text" id="email" class="form-control" placeholder="Your email address">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+					</div>
+				</div>
+				<div class="form-group">
+					<input type="submit" value="Send Message" class="btn btn-primary">
+				</div>
+
+			</form>	
+		</div>
+	</div>
+
+	
+	
+	<div id="fh5co-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>&copy;All Rights Reserved.Designed by FDA</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="/assets/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="/assets/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="/assets/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="/assets/js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="/assets/js/jquery.stellar.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src="/assets/js/jquery.easypiechart.min.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="/assets/js/google_map.js"></script>
+	
+	<!-- Main -->
+	<script src="/assets/js/main.js"></script>
+
+	</body>
+</html>
+
